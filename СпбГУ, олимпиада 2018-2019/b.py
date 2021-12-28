3ea127b2de1b42442375349ced9e208ec37f1a4d
ivgl <49949308+ivgl@users.noreply.github.com>
2021-12-28 20:22:23 +0300

    Add files via upload

---
+++ СпбГУ, олимпиада 2018-2019/b.py
@@ -0,0 +1,13 @@
+﻿import math
+
+a = int(input())
+b = int(input())
+
+if math.sqrt(a+2*b) % 1 == 0:
+	print(4*(math.sqrt(a+2*b)))
+else:
+	if ((math.floor(math.sqrt(a+2*b)))*(math.ceil(math.sqrt(a+2*b))) >= a+2*b):
+		print(2*(math.floor(math.sqrt(a+2*b)))+2*(math.ceil(math.sqrt(a+2*b))))
+	else:
+		print(4*(math.ceil(math.sqrt(a+2*b))))
+	
---
+++ СпбГУ, олимпиада 2018-2019/d.py
@@ -0,0 +1,20 @@
+﻿def f(b,c):
+    s = 0
+    for i in range(0,min(len(b),len(c))):
+        if b[i] == c[i]:
+            s += 1
+        else:
+            break
+    return s
+
+n = int(input())
+for j in range(n):
+  a,b,c = [str(i) for i in input().split()]
+  a = int(a)
+
+  if (len(b) + len(c) - 2*(f(b,c))) % 2 == a % 2:
+      print("Yes")
+  else:
+      print("No") 
+
+	
